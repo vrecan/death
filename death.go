4b77e2be5aa4d4baef4d93e050b6a5b131588349
Ben Aldrich <vrecan@gmail.com>
2015-03-09 08:35:43 -0600

    initial commit needs testing

---
+++ death.go
@@ -0,0 +1,86 @@
+package death
+
+import (
+	"fmt"
+	"os"
+	"os/signal"
+	"sync"
+)
+
+type SIGNAL string
+
+const (
+	TERMINATED SIGNAL = "terminated"
+	INTERRUPT  SIGNAL = "interrupt"
+	UNKNOWN    SIGNAL = "unknown"
+)
+
+type Death struct {
+	wg           sync.WaitGroup
+	sigChannel   chan os.Signal
+	deathChannel chan string
+	deathSignals map[SIGNAL]bool
+}
+
+func NewDeath() (death *Death) {
+	death = &Death{}
+	death.sigChannel = make(chan os.Signal, 10)
+	death.deathChannel = make(chan string, 10)
+	signal.Notify(death.sigChannel)
+	go listenForSignal(death.sigChannel, death.deathChannel)
+	return death
+}
+
+func (d *Death) SetDeathSignals(signals ...SIGNAL) {
+	for _, sig := range signals {
+		d.deathSignals[sig] = true
+	}
+}
+
+func (d *Death) RemoveDeathSignals(signals ...SIGNAL) {
+	for _, sig := range signals {
+		delete(d.deathSignals, sig)
+	}
+}
+
+func getSignal(sig string) SIGNAL {
+	switch sig {
+	case string(TERMINATED):
+		{
+			return TERMINATED
+		}
+	case string(INTERRUPT):
+		{
+			return INTERRUPT
+		}
+	default:
+		{
+			fmt.Println("Unknown signal received: ", sig)
+			return UNKNOWN
+		}
+	}
+}
+
+func (d *Death) listenForDeath() {
+	d.wg.Add(1)
+	defer d.wg.Done()
+	for sig := range d.deathChannel {
+		signal := getSignal(sig)
+		_, ok := d.deathSignals[signal]
+		if ok {
+			return
+		}
+	}
+}
+
+//Wait for death
+func (d *Death) WaitForDeath() {
+	d.wg.Wait()
+}
+
+//Manage death of application by signal
+func listenForSignal(c <-chan os.Signal, death chan string) {
+	for sig := range c {
+		death <- sig.String()
+	}
+}
