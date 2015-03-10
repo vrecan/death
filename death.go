f7aa3ecb5ecfebdae3b2db128dac3b802f9681e6
Ben Aldrich <vrecan@gmail.com>
2015-03-09 20:41:45 -0600

    adjust api

--- death.go
+++ death.go
@@ -1,91 +1,48 @@
 package death
 
 import (
-	"fmt"
+	// "fmt"
 	"os"
 	"os/signal"
 	"sync"
 )
 
-type SIGNAL string
-
-const (
-	TERMINATED SIGNAL = "terminated"
-	INTERRUPT  SIGNAL = "interrupt"
-	UNKNOWN    SIGNAL = "unknown"
-)
-
 type Death struct {
-	wg           sync.WaitGroup
-	sigChannel   chan os.Signal
-	deathChannel chan string
-	deathSignals map[SIGNAL]bool
+	wg         sync.WaitGroup
+	sigChannel chan os.Signal
 }
 
-func NewDeath() (death *Death) {
+func NewDeath(signals ...os.Signal) (death *Death) {
 	death = &Death{}
 	death.sigChannel = make(chan os.Signal, 10)
-	death.deathChannel = make(chan string, 10)
-	signal.Notify(death.sigChannel)
-	go listenForSignal(death.sigChannel, death.deathChannel)
+	signal.Notify(death.sigChannel, signals...)
 	return death
 }
 
-func (d *Death) SetDeathSignals(signals ...SIGNAL) {
-	for _, sig := range signals {
-		d.deathSignals[sig] = true
-	}
-}
-
-func (d *Death) RemoveDeathSignals(signals ...SIGNAL) {
-	for _, sig := range signals {
-		delete(d.deathSignals, sig)
-	}
-}
-
-func getSignal(sig string) SIGNAL {
-	switch sig {
-	case string(TERMINATED):
-		{
-			return TERMINATED
-		}
-	case string(INTERRUPT):
-		{
-			return INTERRUPT
-		}
-	default:
-		{
-			fmt.Println("Unknown signal received: ", sig)
-			return UNKNOWN
-		}
-	}
-}
-
-func (d *Death) listenForDeath() {
-	for sig := range d.deathChannel {
-		signal := getSignal(sig)
-		_, ok := d.deathSignals[signal]
-		if ok {
-			return
-		}
-	}
+type Closable interface {
+	Close()
 }
 
 //Wait for death
-func (d *Death) WaitForDeath() {
-	d.listenForDeath()
-}
-
-//Manage death of application by signal
-func listenForSignal(c <-chan os.Signal, death chan string) {
-	for sig := range c {
-		death <- sig.String()
+func (d *Death) WaitForDeath(closable []Closable) {
+	d.wg.Wait()
+	for _, c := range closable {
+		c.Close()
 	}
 }
 
+//Manage death of application by signal
+func (d *Death) listenForSignal(c <-chan os.Signal, death chan string) {
+	d.wg.Add(1)
+	defer d.wg.Done()
+	for _ = range c {
+		return
+	}
+}
+
+//Shutdown.
 func (d *Death) Close() {
 	if nil != d {
 		close(d.sigChannel)
-		close(d.deathChannel)
 	}
 }
